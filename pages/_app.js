70bd92d028bad7be5b5ff4e2b1f70f08b65280bc
pKafski <kafski.patrick@gmail.com>
2026-03-25 19:09:16 -0300

    pagian faq

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
                 <link href="https://fonts.googleapis.com/css2?family=Momo+Signature&display=swap" rel="stylesheet"/> */}
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin={true.toString()}/>
                 <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Caveat:wght@500&family=Momo+Signature&display=swap" rel="stylesheet"/>      
             </Head>
             <GlobalStyle />
